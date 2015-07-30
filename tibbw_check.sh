fe4d183eb88120c561d26dd3348fc8ca457d0801
Hannes Lehmann <hannes.lehmann@nterra.com>
2015-07-30 17:10:44 +0200

    Some cosmetic update and list of processes added

--- tibbw_check.sh
+++ tibbw_check.sh
@@ -2,8 +2,8 @@
 
 ##
 #   Description:    This shell script will print out to the terminal some information about
-#                   running ems instances. Will be enhanced to use for ops guys taking
-#                   over an EMS infrastructure to get a first idea
+#                   running bwengines instances. Will be enhanced to use for ops guys taking
+#                   over an BW infrastructure to get a first idea
 #
 #   Usage:          Run from the command line
 #
@@ -27,40 +27,44 @@ do
         # echo "Item: $item"
         pid=${array[1]}
         bin=${array[10]}
-		for params in "${array[@]}"
-		do
-			 [[ $params == *.tra ]] && tra=$params
-		done
+	for params in "${array[@]}"
+	do
+		 [[ $params == *.tra ]] && tra=$params
+	done
         details=`lsof -c bwengine  2>/dev/null | grep $pid | awk '{split($0,a," "); print a[9]}'`
-		IFS=$'\n'
+	IFS=$'\n'
         heap=`cat $tra | grep "java.heap.size.max=" | awk '{split($0,a,"="); print a[2]}'`
-		domain=`cat $tra | grep "java.property.TIBCO_DOMAIN_NAME=" | awk '{split($0,a,"="); print a[2]}'`
-		repo=`cat $tra | grep "tibco.repourl=" | awk '{split($0,a,"="); print a[2]}'`
-		starters=`cat $tra | grep "Config.Primary.Partitions=" | awk '{split($0,a,"="); print a[2]}' `
+	domain=`cat $tra | grep "java.property.TIBCO_DOMAIN_NAME=" | awk '{split($0,a,"="); print a[2]}'`
+	repo=`cat $tra | grep "tibco.repourl=" | awk '{split($0,a,"="); print a[2]}'`
+	starters=`cat $tra | grep "Config.Primary.Partitions=" | awk '{split($0,a,"="); print a[2]}' `
         # conf=`cat $log | grep "Reading configuration" | tail -1 |  awk '{split($0,a,"'"'"'"); print a[2]}'`
         # name=`cat $conf | grep "server[[:space:]]*=" | awk '{split($0,a,"="); print a[2]}' `
         echo "-------------------------------------------------------------------------------------"
         echo " Product:       Tibco BusinessWorks Engine"
-        echo " Instance:      $COUNTER"
+        echo " Number:        $COUNTER"
         echo " PID:           $pid "
-		echo " Domain:        $domain"
+	echo " Domain:        $domain"
         # echo " Port:          $port"
         echo " Executable:    $bin"
-		echo " TRA:           $tra"
-		echo " Repo:          $repo"
+	echo " TRA:           $tra"
+	echo " Repo:          $repo"
         # echo " Logfile:       $log"
         # echo " Config:        $conf"
         # echo " Name:         $name"
-		for params in $details
-		do
-			 [[ $params == *.log ]] && echo " Logfile:       $params"
-		done
-		echo " HEAP:          $heap"
-		IFS=',' read -a array <<< "$starters"
-		for starter in "${array[@]}"
-		do
-			 echo " Proz. Starter: $starter"
-		done
+	for params in $details
+	do
+		 [[ $params == *.log ]] && echo " Logfile:       $params"
+	done
+	echo " HEAP:          $heap"
+	IFS=',' read -a array <<< "$starters"
+	for starter in "${array[@]}"
+	do
+		 echo " Proc. Starter: $starter"
+	done
+	for process in `find  $repo | grep ".process"`
+	do
+		echo " Process:       $process"
+	done 
         echo
         let COUNTER+=1 
 done
